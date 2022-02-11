36d00514332bcec326d3b3627a31c31858cc49a9
Kazato Sugimoto <uiureo@gmail.com>
2022-02-11 18:50:48 +0900

    first commit

---
+++ lib/base_serializer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "base_serializer/version"
+
+module BaseSerializer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/base_serializer/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module BaseSerializer
+  VERSION = "0.1.0"
+end
